1921ecd29b6af35b7afc40be153f339ff22c8635
lucnguyendinh <lucnguyendinh202@gmail.com>
2023-07-25 15:57:48 +0700

    custime time

--- src/config/timeDefault.ts
+++ src/config/timeDefault.ts
@@ -9,13 +9,9 @@ const timeDefault = (time: number) => {
     } else if (time < 2592000) {
         return (displayTime = `${Math.floor(time / 60 / 60 / 24)} ngày trước`)
     } else if (time < 77760000) {
-        return (displayTime = `${Math.floor(
-            time / 60 / 60 / 24 / 30,
-        )} tháng trước`)
+        return (displayTime = `${Math.floor(time / 60 / 60 / 24 / 30)} tháng trước`)
     } else {
-        return (displayTime = `${Math.floor(
-            time / 60 / 60 / 24 / 30 / 12,
-        )} năm trước trước`)
+        return (displayTime = `${Math.floor(time / 60 / 60 / 24 / 30 / 12)} năm trước trước`)
     }
 }
 
